701f232fd068a0a83a845778507a269a72ead71c
b <vpbalint@silentsignal.hu>
2025-01-15 18:26:00 +0100

    ob 2019

--- ob_2019.py
+++ ob_2019.py
@@ -11,18 +11,18 @@ def print_hex(input):
 def obfuscate_1470(t0, t1, plain):
     ret = []
     for i, p in enumerate(plain):
-        val0 = (p + (t1[i % 7])) % 256
+        val0 = (p + (t1[i & 7])) % 256
         val1 = (val0 ^ t0[i % 7]) % 256
         # sys.stdout.write("%02X " % (val1))
         ret.append(val1)
     return ret
 
 
-def deobfuscate_1471(t0, t1, cipher):
+def deobfuscate_1470(t0, t1, cipher):
     ret = []
     for i, p in enumerate(cipher):
         val1 = (p ^ t0[i % 7]) % 256
-        val0 = (val1 - (t1[i % 8])) % 256
+        val0 = (val1 - (t1[i & 7])) % 256
         # sys.stdout.write("%02X " % (val1))
         ret.append(val0)
     return ret
@@ -36,27 +36,27 @@ def obfuscate(t0, t1, host1473, val1472, plain):
     return round1
 
 
-def deobfuscate(host1473, val1472, cipher):
-    round1 = deobfuscate_1471(val1472, host1473, cipher)
-    # print_hex(round1)
+def deobfuscate(host, obkey, cipher):
+    round1 = deobfuscate_1470(obkey, host, cipher)
+    #print_hex(round1)
     part0 = round1[0:4]
     part1 = round1[4:8]
     cipher1 = round1[8:]
     t0 = part0 + part1
     t1 = part1 + part0
 
-    return deobfuscate_1471(t0, t1, cipher1)
+    return deobfuscate_1470(t0, t1, cipher1)
 
 def gen_obfuscator_key(build_guid, product_id):
     ret=[0,0,0,0,0,0,0,0]
     machine_id=list(build_guid.encode('utf-16le'))
     machine_id.extend(list(product_id.encode('utf-16le')))
-    print("Machine ID [%d]:" % (len(machine_id)))
-    print_hex(machine_id)
+    #print("Machine ID [%d]:" % (len(machine_id)))
+    #print_hex(machine_id)
     idx=0
     for i in range(0, len(machine_id), 2):
-        print(bytes(machine_id[i:]).decode("utf-16le"), i, hex(machine_id[i]))
-        print_hex(ret)
+        #print(bytes(machine_id[i:]).decode("utf-16le"), i, hex(machine_id[i]))
+        #print_hex(ret)
         ret[idx] = ret[idx] ^ machine_id[i]
         idx = (idx + 1) & 7
     return ret
@@ -71,26 +71,17 @@ def test():
     
     round0 = obfuscate_1470(t0, t1, plain)
     print_hex(round0)
-    
-    obkey=gen_obfuscator_key("ffffffff-ffff-ffff-ffff-ffffffffffff", "00431-10000-00000-AA321")
-    print_hex(obkey)
-    """
-    ob = obfuscate(
-        t0, t1, "WIN-N6MF".encode("ascii"), [0x63, 0x45, 0, 0, 0, 0, 0x45, 0x63], plain
-    )
-
-    deob = deobfuscate(
-        "WIN-N6MF".encode("ascii"), [0x63, 0x45, 0, 0, 0, 0, 0x45, 0x63], ob
-    )
-
-    print(bytes(deob).decode("utf-16le"))
-    """
-
+    print_hex(deobfuscate_1470(t0,t1,round0))
+   
 def main():
     # Dumb parsing of Registry export
     fat_re = re.compile('"Function Admin Timestamp"=hex:[0-9a-f,\\\\ \n]*', re.DOTALL)
-    hostname = sys.argv[1]
-    reg_data = open(sys.argv[2], "r", encoding="utf-16le").read()
+    hostname = sys.argv[1][0:8]
+    build_guid= sys.argv[2]
+    product_id=sys.argv[3]
+    #obkey=gen_obfuscator_key("ffffffff-ffff-ffff-ffff-ffffffffffff", "00431-10000-00000-AA321")
+    obkey=gen_obfuscator_key(build_guid, product_id)
+    reg_data = open(sys.argv[4], "r", encoding="utf-16le").read()
     fat_line = fat_re.search(reg_data)
     fat_hex = (
         fat_line.group(0)
@@ -100,33 +91,20 @@ def main():
         .replace(" ", "")
     )
     fat_bytes = [int(b, 16) for b in fat_hex.split(",")]
-    # print_hex(fat_bytes)
-
-    # Brute-Force val1472
-    for b0 in range(0, 256):
-        for b1 in range(0, 256):
-            deob = deobfuscate(
-                hostname.encode("ascii"), [b0, b1, 0, 0, 0, 0, b1, b0], fat_bytes
-            )
-
-            # Heuristic: we look for typeable UTF-16LE
-            found = True
-            for i in range(1, 17, 2):
-                if deob[i] != 0:
-                    found = False
-                    break
-            if not found:
-                continue
-            for i in range(0, 16, 2):
-                if deob[i] < 0x1F or deob[i] > 0x7E:
-                    found = False
-                    break
-            if found:
-                print(
-                    "[+] Found candidate with key (%02X, %02X): %s"
-                    % (b0, b1, bytes(deob[0:16]).decode("utf-16le"))
-                )
+    #fat_bytes=[0x59, 0x42, 0xc8, 0xf2, 0x07, 0x1e, 0x7a, 0x48, 
+    #           0x46, 0xd4, 0x4c, 0xac, 0xfd, 0xf3, 0x76, 0x9e]
+    #print_hex(obkey)
+    deob = deobfuscate(
+            hostname.encode("ascii"), obkey, fat_bytes
+        )
+    print_hex(deob)
+    password_bytes=[]
+    for i in range(0,len(deob),2):
+        if deob[i]==0:
+            break
+        password_bytes.append(deob[i])
+    print("Password: %s" % (bytes(password_bytes).decode("ascii")))
 
 
-#main()
-test()
+main()
+#test()
